4f6ae0926a237489e64223c05e124f0078f7c9fe
Jorge Hernandez <jorge.hernandez@dreamcodesoft.com>
2021-05-06 17:05:19 -0500

    .

--- src/controller/controller1.js
+++ src/controller/controller1.js
@@ -1,5 +1,12 @@
-function helloWorld(){
+const path = require('path');
+
+async function helloWorld(request, response){
     console.log("Hellor world since node")
+    response.sendFile(path.resolve('src/views/index.html'))
 }
 
-module.exports = {helloWorld}
+async function addItems(request,response){
+    response.sendFile(path.resolve('src/views/add-item.html'))
+}
+
+module.exports = {helloWorld,addItems}
--- src/server/routes.js
+++ src/server/routes.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router();
 const myFunctions = require("../controller/controller1");
 router.route("/").get(myFunctions.helloWorld);
-router.route("/add-item").get()
+router.route("/add-item").get(myFunctions.addItems);
 module.exports = {router}
