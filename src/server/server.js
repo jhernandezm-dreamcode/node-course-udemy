204cd34ebd446294986c01c6fb45801d2d15ac7c
Jorge Hernandez <jorge.hernandez@dreamcodesoft.com>
2021-06-08 15:49:32 -0500

    .

--- src/server/server.js
+++ src/server/server.js
@@ -3,8 +3,10 @@ const server = express();
 const routes = require("../server/routes");
 const middleware = require("../middlewares/middlewares");
 const router = routes.router;
+const cors = require("cors")
 
 server.use(express.json()) 
 server.use('/v1',router);
 server.use(middleware.errorView);
+server.use(cors())
 module.exports = {server}
